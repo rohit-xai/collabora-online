76fc2d1e91391bf5c566c0ede7868e0b9061ce87
rohit-xai <143767902+rohit-xai@users.noreply.github.com>
2024-06-19 14:05:11 +1000

    Update TileDesc.hpp

--- wsd/TileDesc.hpp
+++ wsd/TileDesc.hpp
@@ -131,6 +131,11 @@ public:
                _mode == other._mode;
     }
 
+    bool operator!=(const TileDesc& other) const
+    {
+        return !(*this == other);
+    }
+
     // used to cache a hash of the key elements compared in ==
     uint32_t equalityHash() const
     {
